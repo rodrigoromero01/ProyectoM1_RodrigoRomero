c17ceb784494d8fa3871db9e622fc11ad38d5915
rodrigoromero01 <rodrigoromero01@gmail.com>
2026-07-17 13:25:21 -0300

    Refactorizacion funcion generarColor y arreglo path arhivo .js en el index

--- script.js
+++ script.js
@@ -1,2 +1,52 @@
-console.log("hola mundo")
+const botonGenerar = document.getElementById("generar");
+
+const selectorCantidad = document.getElementById("cantidad");
+
+const contenedorPaleta = document.getElementById("paleta");
+
+const toast = document.getElementById("toast");
+
+botonGenerar.addEventListener("click", generarPaleta);
+
+// ===== FUNCIONES =====
+
+function generarPaleta(){
+
+    contenedorPaleta.innerHTML = "";
+
+    const color = generarColor();
+
+    const tarjeta = document.createElement("div");
+
+    tarjeta.className = "color-card";
+
+    const preview = document.createElement("div");
+
+    preview.className = "color-preview";
+
+    preview.style.backgroundColor = color;
+
+    tarjeta.appendChild(preview);
+
+    contenedorPaleta.appendChild(tarjeta);
+
+}
+
+function generarColor() {
+
+    const h = Math.floor(Math.random() * 360);
+
+    const s = Math.floor(Math.random() * 41) + 60;
+
+    const l = Math.floor(Math.random() * 31) + 35;
+
+    const colorHSL = `hsl(${h}, ${s}%, ${l}%)`;
+
+    return colorHSL;
+
+}
+
+
+
+
 
